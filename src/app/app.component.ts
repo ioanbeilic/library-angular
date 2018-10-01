e6a4ba6caee104ebe0f9d8e36f2c85509800ebc4
Ioan Beilic <ioanbeilic@gmail.com>
2018-10-01 12:22:02 +0200

    default nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+export interface test {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'nav-bar';
+  title = "nav-bar";
+
+  test: test[] = [
+    { text: "One", cols: 3, rows: 1, color: "lightblue" },
+    { text: "Two", cols: 1, rows: 2, color: "lightgreen" },
+    { text: "Three", cols: 1, rows: 1, color: "lightpink" },
+    { text: "Four", cols: 2, rows: 1, color: "#DDBDF1" }
+  ];
+
+  navLinks = [
+    { linkName: "link 1 more text", icon: "tag", url: "https://google.com" },
+    { linkName: "link 2 text", icon: "offline_bolt", url: "test" },
+    { linkName: "link 3", icon: "home", url: "test1" },
+    { linkName: "link 4", icon: "", url: "test2" },
+    { linkName: "link 5", icon: "menu", url: "test3" }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { UrbNavBarModule } from "projects/urb-nav-bar/src/public_api";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    UrbNavBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
